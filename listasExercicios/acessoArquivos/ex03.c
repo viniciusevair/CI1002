f21c11249449853a1cae458446f1aa3e56e67f15
Vinicius Evair <vsilva.evair@gmail.com>
2023-04-04 19:12:31 -0300

    Add unfinished ex03

---
+++ listasExercicios/acessoArquivos/ex03.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LINEMAX 1024
+
+int main () {
+    FILE *srce, *dest;
+    char *s;
+
+    if (! (s = calloc(LINEMAX, sizeof(char)))) {
+        perror ("Erro ao alocar memória.");
+        exit(1);
+    }
+    if (! (srce = fopen("minusc.txt", "r"))) {
+        perror("Erro ao abrir o arquivo.");
+        exit (1);
+    }
+    if (! (dest = fopen("maiusc.txt", "w"))) {
+        perror("Erro ao criar o arquivo.");
+        exit (1);
+    }
+
+    fgets(s, LINEMAX, srce);
+    while (! feof(srce)) {
+        for (int i = 0; i < strlen(s); i++)
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                s[i] -= 32;
+            }
+        fgets(s, LINEMAX, srce);
+    }
+
+    free(s);
+    fclose(srce);
+    return 0;
+}
+
