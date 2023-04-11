953516a8cc77d3a80ff345c8860874f44c4f7e38
Vinicius Evair <vsilva.evair@gmail.com>
2023-04-11 10:52:13 -0300

    Renamed exercise 01.a file

---
+++ listasExercicios/binario/ex01a.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    FILE *arq;
+    int i, ret, qtd;
+    long *value;
+
+    srand(time(NULL));
+    if (! (arq = fopen("numeros.bin", "a"))) {
+        perror("Erro ao abrir arquivo");
+        exit(1);
+    }
+
+    qtd = rand() % (10000 - 100 + 1) + 100;
+    if (! (value = calloc(qtd, sizeof(long))))
+        return 1;
+
+    for (i = 0; i < qtd; i++)
+        value[i] = (rand() << 2) + (rand());
+
+    ret = fwrite(value, sizeof(long), qtd, arq);
+
+    if (ret)
+        printf("Gravou %d valores!\n", ret);
+    else
+        printf("Erro ao gravar!\n");
+
+    fclose(arq);
+    free(value);
+    return 0;
+}
