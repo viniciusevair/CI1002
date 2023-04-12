c68d50cd9c517b9ecff49f0d8306bef1c58854e8
Vinicius Evair <vsilva.evair@gmail.com>
2023-04-12 09:37:00 -0300

    Add finished extra exercise

---
+++ listasExercicios/binario/poema.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+long tamanhoPoema() {
+    struct stat st;
+    stat("poema.bin", &st);
+
+    return st.st_size / 5;
+}
+
+void organizaPoema(FILE *arq, char *poema) {
+    int pos;
+    char letra;
+
+    fread(&pos, sizeof(int), 1, arq);
+    fread(&letra, sizeof(char), 1, arq);
+    printf("A primeira posição é %d, e a letra dessa posição é a letra %c\n", pos, letra);
+    while (! feof(arq)) {
+        poema[pos] = letra;
+
+        fread(&pos, sizeof(int), 1, arq);
+        fread(&letra, sizeof(char), 1, arq);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    FILE *arq;
+    char *poema;
+
+    if (! (arq = fopen("poema.bin", "r+"))) {
+        perror("Erro ao abrir arquivo");
+        exit(1);
+    }
+
+    if (! (poema = calloc(tamanhoPoema(), sizeof(char))))
+        return 1;
+
+    organizaPoema(arq, poema);
+
+    printf("%s\n", poema);
+
+    free(poema);
+    fclose(arq);
+    return 0;
+}
