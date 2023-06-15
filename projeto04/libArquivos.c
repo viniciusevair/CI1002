06acb180639b6ecb8bc0d6dc0dfb1084bbce507c
Vinicius Evair <vsilva.evair@gmail.com>
2023-06-15 04:37:35 -0300

    Small progress
    Just some initial tests

--- projeto04/libArquivos.c
+++ projeto04/libArquivos.c
@@ -1,6 +1,9 @@
 #include "libArquivos.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
 
 FILE* cria_arquivador(FILE *archive, char *filename) {
     archive = fopen(filename, "w+");
@@ -30,6 +33,40 @@ FILE* abre_membro(char *filename) {
         fprintf(stderr, "Erro ao abrir arquivo %s.", filename);
 
     return membro;
+}
+
+struct file_header_t* pega_dados(char *filename) {
+    struct file_header_t *file_data;
+    char *aux;
+    struct stat *data_buffer;
+
+    if(! (file_data = malloc(sizeof(struct file_header_t)))) {
+        fprintf(stderr, "Erro ao alocar memória");
+        return NULL;
+    }
+    if(! (data_buffer = malloc(sizeof(struct stat)))) {
+        fprintf(stderr, "Erro ao alocar memória");
+        return NULL;
+    }
+
+    stat(filename, data_buffer);
+    aux = strrchr(filename, '/');
+    if(! aux)
+        aux = strdup(filename);
+    else
+        aux = strdup(aux + 1);
+
+    strcpy(file_data->filename, aux);
+    file_data->modif_date = 8;
+    file_data->permissions = 1;
+    file_data->group_id = 7;
+    file_data->archive_position = 90;
+    file_data->user_id = 666;
+    file_data->size = 30;
+    strcpy(file_data->filepath, "oi");
+
+    free(data_buffer);
+    return file_data;
 }
 
 //TODO:
--- projeto04/libArquivos.h
+++ projeto04/libArquivos.h
@@ -2,8 +2,24 @@
 #define __ARQUIVOS
 
 #include <stdio.h>
+#include <sys/stat.h>
+
+#define NAME_MAX 255
+#define PATH_MAX 4096
+
+struct file_header_t {
+    char filename[NAME_MAX];
+    char filepath[PATH_MAX];
+    size_t user_id;
+    size_t group_id;
+    size_t permissions;
+    size_t size;
+    size_t modif_date;
+    unsigned int archive_position;
+};
 
 FILE* abre_arquivador(char *filename);
 FILE* abre_membro(char *filename);
+struct file_header_t* pega_dados(char *filename);
 
 #endif
