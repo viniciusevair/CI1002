3a1a1e65c5cb07d3ee16aa5f843efe6e27fefac0
Vinicius Evair <vsilva.evair@gmail.com>
2023-06-17 06:29:04 -0300

    Changed test.c to test linked list library

--- projeto04/test.c
+++ projeto04/test.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
+#include "libLista.h"
 #include "libArquivos.h"
 
 int main(int argc, char *argv[]) {
-    FILE *arq;
-    struct file_header_t *teste1, *teste2;
-    if(! (teste1 = malloc(sizeof(struct file_header_t)))) {
-        fprintf(stderr, "Erro ao alocar memória");
-        exit(EXIT_FAILURE);
-    }
-    if(! (teste2 = malloc(sizeof(struct file_header_t)))) {
-        fprintf(stderr, "Erro ao alocar memória");
-        exit(EXIT_FAILURE);
+    struct lista_t *lista = cria_lista();
+
+    struct file_header_t *file0 = pega_dados(argv[2]);
+    struct file_header_t *file1 = pega_dados(argv[3]);
+    struct file_header_t *file2 = pega_dados(argv[4]);
+    struct file_header_t *file3 = pega_dados(argv[5]);
+    struct file_header_t *file4 = pega_dados(argv[6]);
+    struct file_header_t *file5 = pega_dados(argv[7]);
+
+    adiciona_inicio_lista(lista, file0);
+    adiciona_fim_lista(lista, file1);
+    adiciona_ordem_lista(lista, file2);
+    adiciona_fim_lista(lista, file3);
+    adiciona_fim_lista(lista, file4);
+    adiciona_fim_lista(lista, file5);
+
+    printf("Elementos da lista:\n");
+    ler_elementos_lista(lista);
+
+    struct file_header_t *primeiro = obtem_primeiro_lista(lista);
+    if (primeiro != NULL) {
+        printf("Primeiro elemento removido: %s\n", primeiro->filename);
     }
 
-    arq = abre_arquivador(argv[1]);
-    teste1 = pega_dados(argv[2]);
-    fwrite(teste1, sizeof(struct file_header_t), 1, arq);
-    free(teste1);
+    printf("Elementos atualizados da lista:\n");
+    ler_elementos_lista(lista);
 
-    rewind(arq);
-    fread(teste2, sizeof(struct file_header_t), 1, arq);
-    printf("TESTE:\n");
-    printf("Filename: %s\nModif date: %zu\n", teste2->filename, teste2->modif_date);
-    printf("MAIS TESTES:\n");
-    printf("Filepath: %s\nUID: %zu\n", teste2->filepath, teste2->user_id);
+    printf("Removendo um elemento qualquer.\n");
+    remove_arquivo(lista, file4->filename);
+
+    printf("Elementos atualizados da lista:\n");
+    ler_elementos_lista(lista);
+
+    destroi_lista(lista);
 
     return 0;
 }
