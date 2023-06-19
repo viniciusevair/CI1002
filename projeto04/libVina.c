621a0b60270fcab5a44a7f002f669590eb3550e0
Vinicius Evair <vsilva.evair@gmail.com>
2023-06-19 05:51:59 -0300

    Reorganized file for better visualization

--- projeto04/libVina.c
+++ projeto04/libVina.c
@@ -5,7 +5,9 @@
 #include "libVina.h"
 
 #define ARGUMENT_OFFSET 3
+#define BUFFER_SIZE 1024
 
+/* -------- Utilities -------- */
 struct list_t* load_list(FILE *archive, struct list_t *list, size_t archive_pointer) {
     fseek(archive, archive_pointer, SEEK_SET);
     struct file_header_t temp;
@@ -28,24 +30,35 @@ struct list_t* store_list(FILE *archive, struct list_t *list, size_t archive_poi
     return list;
 }
 
+void process_file(FILE *input, FILE *output, size_t file_size) {
+    size_t buffer_size, read_size, bytes_read;
+    buffer_size = BUFFER_SIZE;
+    char buffer[buffer_size];
+
+    while (file_size > 0) {
+        read_size = (file_size < buffer_size)? file_size : buffer_size;
+        bytes_read = fread(buffer, sizeof(char), read_size, input);
+        fwrite(buffer, sizeof(char), bytes_read, output);
+        file_size -= bytes_read;
+    }
+}
+
+/* -------- Update -------- */
+void update_operation(FILE *archive, char **argv, int members_quantity) {
+}
+
+/* -------- Insert -------- */
 void insert_file(FILE *archive, struct list_t *list, char *filename, size_t *archive_pointer) {
     struct file_header_t *file_data = get_data(filename);
     printf("TESTE: %s\n", filename);
     FILE *member = open_member(filename);
     size_t file_size = file_data->size;
-    size_t buffer_size = 1024;
-    char buffer[buffer_size];
     file_data->archive_position = *archive_pointer;
 
     if(member == NULL)
         return;
 
-    while (file_size > 0) {
-        size_t read_size = (file_size < buffer_size)? file_size : buffer_size;
-        size_t bytes_read = fread(buffer, sizeof(char), read_size, member);
-        fwrite(buffer, sizeof(char), bytes_read, archive);
-        file_size -= bytes_read;
-    }
+    process_file(member, archive, file_size);
 
     (*archive_pointer) += file_data->size;
     add_list_tail(list, file_data);
@@ -69,6 +82,7 @@ void insert_operation(FILE *archive, char **argv, int members_quantity) {
             insert_file(archive, list, argv[i], &archive_pointer);
     //TODO: fazer o caso de o arquivo já estar lá dentro. Acho que é mais fácil
     //com a remoção já feita depois.
+
     fseek(archive, 0, SEEK_SET);
     fwrite(&archive_pointer, sizeof(size_t), 1, archive);
 
@@ -76,29 +90,20 @@ void insert_operation(FILE *archive, char **argv, int members_quantity) {
     list = delete_list(list);
 }
 
-void update_file(FILE *archive, char **argv, int members_quantity) {
-}
-
+/* -------- Move -------- */
 void move_file(FILE *archive, char **argv, int members_quantity) {
 }
 
+/* -------- Extract -------- */
 void extract_file(FILE *archive, struct file_header_t *file_data) {
     FILE *member = make_member(file_data->filename);
     size_t file_size = file_data->size;
-    size_t buffer_size = 1024;
-    char buffer[buffer_size];
 
     if(member == NULL)
         return;
 
     fseek(archive, file_data->archive_position, SEEK_SET);
-
-    while (file_size > 0) {
-        size_t read_size = (file_size < buffer_size)? file_size : buffer_size;
-        size_t bytes_read = fread(buffer, sizeof(char), read_size, archive);
-        fwrite(buffer, sizeof(char), bytes_read, member);
-        file_size -= bytes_read;
-    }
+    process_file(archive, member, file_size);
 
     fclose(member);
 }
@@ -128,9 +133,11 @@ void extract_operation(FILE *archive, char **argv, int members_quantity) {
     }
 }
 
+/* -------- Remove -------- */
 void remove_operation(FILE *archive, char **argv, int members_quantity) {
 }
 
+/* -------- List -------- */
 void list_files(FILE *archive) {
     size_t archive_pointer;
     fread(&archive_pointer, sizeof(size_t), 1, archive);
@@ -141,6 +148,7 @@ void list_files(FILE *archive) {
         write_file_data(&temp);
 }
 
+/* -------- Help -------- */
 void help_utility(char *call) {
     printf("Uso:\n");
     printf("%s [-i | -a | -m <target> | -x | -r | -c | -h]", call);
